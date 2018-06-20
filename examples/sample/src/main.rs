71b9ac19ac36c2e6088e1a7c40b3de03701d71aa
gfreezy <gfreezy@gmail.com>
2018-06-20 20:47:48 +0800

    track example

---
+++ examples/sample/src/main.rs
@@ -0,0 +1,166 @@
+#![feature(nll)]
+extern crate bitcask_rs;
+extern crate actix_web;
+extern crate actix;
+extern crate failure;
+extern crate futures;
+#[macro_use]
+extern crate serde_derive;
+
+use failure::Error;
+use futures::future::{Future, err};
+use actix_web::{server, App, HttpRequest, Responder, FutureResponse, HttpResponse, AsyncResponder, Query, FromRequest};
+use std::path::PathBuf;
+use actix::prelude::{Message, Actor, SyncContext, Handler, SyncArbiter, Addr, Syn};
+
+
+#[derive(Deserialize)]
+struct Info {
+    key: String,
+    value: Option<String>,
+}
+
+
+fn index(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
+    let state = req.state();
+    let addr: &Addr<Syn, BitcaskActor> = &state.addr;
+    addr.send(List).from_err()
+        .map(move |ret|
+            match ret {
+                Ok(v) => HttpResponse::Ok().body(format!("{:?}", v)),
+                Err(e) => HttpResponse::BadRequest().body(format!("{}", e))
+            }).responder()
+}
+
+fn get(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
+    let key = match Query::<Info>::extract(&req) {
+        Ok(v) => v,
+        Err(e) => return Box::new(err(e))
+    };
+    let state = req.state();
+    let addr: &Addr<Syn, BitcaskActor> = &state.addr;
+    addr.send(Get(key.key.clone())).from_err()
+        .map(move |ret|
+            match ret {
+                Ok(v) => HttpResponse::Ok().body(format!("{}: {:?}", &key.key, v)),
+                Err(e) => HttpResponse::BadRequest().body(format!("{}", e))
+            }).responder()
+}
+
+fn set(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
+    let info = match Query::<Info>::extract(&req) {
+        Ok(v) => v,
+        Err(e) => return Box::new(err(e))
+    };
+    let state = req.state();
+    let addr: &Addr<Syn, BitcaskActor> = &state.addr;
+    addr.send(Set(info.key.clone(), info.value.as_ref().map_or(vec![0], |v| v.as_bytes().to_vec()))).from_err()
+        .map(|ret|
+            match ret {
+                Ok(_) => HttpResponse::Ok().body("ok"),
+                Err(e) => HttpResponse::BadRequest().body(format!("{}", e))
+            }).responder()
+}
+
+fn delete(req: HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
+    let info = match Query::<Info>::extract(&req) {
+        Ok(v) => v,
+        Err(e) => return Box::new(err(e))
+    };
+    let state = req.state();
+    let addr: &Addr<Syn, BitcaskActor> = &state.addr;
+    addr.send(Delete(info.key.clone())).from_err()
+        .map(|ret|
+            match ret {
+                Ok(_) => HttpResponse::Ok().body("ok"),
+                Err(e) => HttpResponse::BadRequest().body(format!("{}", e))
+            }).responder()
+}
+
+struct List;
+
+struct Get(String);
+
+struct Set(String, Vec<u8>);
+
+struct Delete(String);
+
+impl Message for List {
+    type Result = Result<Option<Vec<String>>, Error>;
+}
+
+impl Message for Get {
+    type Result = Result<Option<Vec<u8>>, Error>;
+}
+
+impl Message for Set {
+    type Result = Result<(), Error>;
+}
+
+impl Message for Delete {
+    type Result = Result<(), Error>;
+}
+
+
+struct BitcaskActor(bitcask_rs::Bitcask);
+
+impl Actor for BitcaskActor {
+    type Context = SyncContext<Self>;
+}
+
+impl Handler<Get> for BitcaskActor {
+    type Result = Result<Option<Vec<u8>>, Error>;
+
+    fn handle(&mut self, msg: Get, _: &mut Self::Context) -> Self::Result {
+        self.0.get(msg.0)
+    }
+}
+
+
+impl Handler<Set> for BitcaskActor {
+    type Result = Result<(), Error>;
+
+    fn handle(&mut self, msg: Set, _: &mut Self::Context) -> Self::Result {
+        self.0.set(msg.0, msg.1)
+    }
+}
+
+impl Handler<Delete> for BitcaskActor {
+    type Result = Result<(), Error>;
+
+    fn handle(&mut self, msg: Delete, _: &mut Self::Context) -> Self::Result {
+        self.0.delete(msg.0)
+    }
+}
+
+impl Handler<List> for BitcaskActor {
+    type Result = Result<Option<Vec<String>>, Error>;
+
+    fn handle(&mut self, msg: List, ctx: &mut Self::Context) -> <Self as Handler<List>>::Result {
+        Ok(Some(self.0.keys().into_iter().map(|k| k.clone()).collect::<Vec<String>>()))
+    }
+}
+
+struct AppState {
+    addr: Addr<Syn, BitcaskActor>
+}
+
+
+fn main() {
+    let config = bitcask_rs::ConfigBuilder::default().path(PathBuf::from("target/store3")).build().unwrap();
+    let bitcask = bitcask_rs::Bitcask::new(config);
+    let sys = actix::System::new("hello-world");
+
+    let addr = SyncArbiter::start(2, move || BitcaskActor(bitcask.clone()));
+
+    server::new(move ||
+        App::with_state(AppState { addr: addr.clone() })
+            .resource("/", |r| r.f(index))
+            .resource("/get", |r| r.route().a(get))
+            .resource("/set", |r| r.route().a(set))
+            .resource("/delete", |r| r.route().a(delete))
+    ).bind("127.0.0.1:8088")
+        .unwrap()
+        .start();
+    sys.run();
+}
